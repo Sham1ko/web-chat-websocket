fdf5e962715b80d6c712474674a56e05ee8c5d55
Shamshyrak Zholdasbek <shamshyrak.zholdasbek@gmail.com>
2023-06-12 15:50:46 +0600

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const ChatApp = () => {
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState("");
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:3000");
+    setSocket(newSocket);
+
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("message", (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      });
+
+      socket.emit("getMessageHistory");
+      socket.on("messageHistory", (history) => {
+        setMessages(history);
+      });
+    }
+  }, [socket]);
+
+  const handleSendMessage = () => {
+    if (messageInput.trim() !== "") {
+      socket.emit("message", messageInput);
+      setMessageInput("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Chat App</h1>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <strong>{message.sender}: </strong>
+            <span>{message.content}</span>
+          </div>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatApp;
